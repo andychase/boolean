aaf934dc37f1830292c3ed9168945ba2ff2206b6
Scisco <scisco7@gmail.com>
2015-10-27 12:37:11 -0400

    add classsifiers

--- setup.py
+++ setup.py
@@ -28,4 +28,15 @@ setup(name='boolean',
       include_package_data=True,
       zip_safe=False,
       test_suite="test",
+      classifiers=[
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
       )
